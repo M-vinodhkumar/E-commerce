f6a72d5ba14ec708302e83d23ce7317c587eadce
M-vinodhkumar <ms9652055@gmail.com>
2025-09-28 17:56:19 +0530

    crt

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,18 +3,18 @@ import { FaCamera, FaVideo, FaCogs, FaCompactDisc } from "react-icons/fa";
 
 const Home = ({ addToCart, buyNow }) => {
     const products = [
-        { id: 1, name: "Canon EOS R50", price: 999.0, image: "./Canon.webp", Description: "Canon EOS R50 24.2MP Mirrorless Camera (18-45 mm Lens, 5-Axis Electronic Image Stabilization)" },
-        { id: 2, name: "Sony A7 III", price: 1499.0, image: "./Sony.webp",Description: "SONY Alpha 6700M 26MP Mirrorless Camera (18-135 mm Lens, 23.3 x 15.5 mm Sensor, BIONZ XR Image Processor)" },
-        { id: 3, name: "Nikon 50mm f/1.8", price: 199.0, image: "./Nikon.webp",Description: "Nikon Z 50II 20.9MP Mirrorless Camera (16-50 mm and 50-250 mm Lens, 23.5 x 15.7 mm Sensor, TFT Touch Sensitive LCD)" },
-        { id: 4, name: "Action Camera", price: 299.0, image: "./Action.webp",Description: "GoPro Hero 4K and 12MP 60 FPS Waterproof Action Camera with Rear LCD Touchscreen (Black)" },
-        { id: 5, name: "GoPro", price: 559.0, image: "./Gopro.webp", Description: "GoPro Hero12 5.3K and 27MP 240 FPS Waterproof Action Camera with Voice Control (Black)" },
-        { id: 5, name: "Insta360", price: 349.0, image: "./Insta360.webp", Description: "Insta360 X3 8K and 72MP 60 FPS Waterproof Action Camera with Magic Third Person Views (Black)" },
+        { id: 1, name: "Canon EOS R50", price: 999.0, image: "/src/images/274329_0_upjhd2.webp", Description: "Canon EOS R50 24.2MP Mirrorless Camera (18-45 mm Lens, 5-Axis Electronic Image Stabilization)" },
+        { id: 2, name: "Sony A7 III", price: 1499.0, image: "/src/images/303425_0_Xo4POLg9J.webp",Description: "SONY Alpha 6700M 26MP Mirrorless Camera (18-135 mm Lens, 23.3 x 15.5 mm Sensor, BIONZ XR Image Processor)" },
+        { id: 3, name: "Nikon 50mm f/1.8", price: 199.0, image: "/src/images/312192_cwlo5r.webp",Description: "Nikon Z 50II 20.9MP Mirrorless Camera (16-50 mm and 50-250 mm Lens, 23.5 x 15.7 mm Sensor, TFT Touch Sensitive LCD)" },
+        { id: 4, name: "Action Camera", price: 299.0, image: "/src/images/308770_zbnrsa.webp",Description: "GoPro Hero 4K and 12MP 60 FPS Waterproof Action Camera with Rear LCD Touchscreen (Black)" },
+        { id: 5, name: "GoPro", price: 559.0, image: "/src/images/245216_0_lx8nyh.webp", Description: "GoPro Hero12 5.3K and 27MP 240 FPS Waterproof Action Camera with Voice Control (Black)" },
+        { id: 5, name: "Insta360", price: 349.0, image: "/src/images/263094_0_xmxxzf.webp", Description: "Insta360 X3 8K and 72MP 60 FPS Waterproof Action Camera with Magic Third Person Views (Black)" },
     ];
 
 
     
     return (
-        <div className="bg-gray-100">   
+        <div className="bg-gray-100">
             <section className="relative bg-gray-900 text-white">
                 <div className="max-w-7xl mx-auto px-6 py-20 flex flex-col md:flex-row items-center">
                     <div className="flex-1 space-y-6">
@@ -28,7 +28,7 @@ const Home = ({ addToCart, buyNow }) => {
                         </div>
                     </div>
                     <div className="flex-1 mt-10 md:mt-0">
-                        <img src="/src/images/head.webp" alt="Camera Hero" className="rounded-lg shadow-lg" />
+                        <img src="/src/images/banner1.jpg" alt="Camera Hero" className="rounded-lg shadow-lg" />
                     </div>
                 </div>
             </section>
